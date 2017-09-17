0a2038bb67eebd7b518626bd4c0e931853c81089
IovlevYuliy <iovlev2@gmail.com>
2017-09-17 22:18:22 +0700

    added package.json, app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
