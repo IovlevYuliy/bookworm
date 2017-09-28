e0f09352c8596f6c0b091e9eae2d11c69fce7234
IovlevYuliy <iovlev2@gmail.com>
2017-09-28 23:18:11 +0700

    added downloading of books to initial page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,35 @@
+$('#search-btn')
+    .click(bookSearch);
+
 function bookSearch() {
-	var search = document.getElementById('search').value;
-	var results = document.getElementById('results');
+	var search = $('#search').val();
+	var results = $('#results');
 
 	$.ajax({
 		type: 'GET',
 		url: 'https://www.googleapis.com/books/v1/volumes?q=' + search,
-		dataType: json,
+		dataType: "json",
 
 		success: function(data) {
-			results.innerHTML = data;
+            let container = $('#book-container');
+            for (let i = 0 ; i < data.items.length; ++i) {
+                let book = $('<div>')
+                    .attr({
+                        'class': 'book-item',
+                    });
+
+                let title = $('<span>')
+                    .text(data.items[i].volumeInfo.title)
+                    .appendTo(book);
+
+                let image = $('<img>')
+                    .attr({
+                        'class': 'book-image',
+                        'src': data.items[i].volumeInfo.imageLinks.smallThumbnail,
+                    })
+                    .appendTo(book);
+                container.append(book);
+            }
 		}
 	})
 }
-document.getElementById('search-btn').addEventListener('click', bookSearch, false);
