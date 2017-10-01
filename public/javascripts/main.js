e3773cfd2333e9e43a5344c38cd02264332d1621
IovlevYuliy <iovlev2@gmail.com>
2017-10-01 20:27:29 +0700

    fixed styles

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,31 +3,41 @@ $('#search-btn')
 
 function bookSearch() {
 	var search = $('#search').val();
-	var results = $('#results');
 
+    let container = $('#book-container').empty();
 	$.ajax({
 		type: 'GET',
 		url: 'https://www.googleapis.com/books/v1/volumes?q=' + search,
 		dataType: "json",
 
 		success: function(data) {
-            let container = $('#book-container');
             for (let i = 0 ; i < data.items.length; ++i) {
                 let book = $('<div>')
                     .attr({
                         'class': 'book-item',
                     });
 
-                let title = $('<span>')
-                    .text(data.items[i].volumeInfo.title)
-                    .appendTo(book);
-
                 let image = $('<img>')
                     .attr({
                         'class': 'book-image',
                         'src': data.items[i].volumeInfo.imageLinks.smallThumbnail,
+                    });
+
+                 let divImg = $('<div>')
+                    .attr({
+                        'class': 'book-image-container',
                     })
+                    .height(Math.max(200, image[0].height))
+                    .append(image)
                     .appendTo(book);
+
+                let title = $('<span>')
+                    .attr({
+                        'class': 'book-title',
+                    })
+                    .text(data.items[i].volumeInfo.title)
+                    .appendTo(book);
+
                 container.append(book);
             }
 		}
